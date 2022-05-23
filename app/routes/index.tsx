cfdcff7b9293a8bf22f28a7d2101896fec65a358
Koki Takahashi <hakatasiloving@gmail.com>
2022-05-23 22:03:48 +0000

    Setup lint

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,35 +1,35 @@
-import Youtube from 'react-player/youtube'
+import Youtube from 'react-player/youtube';
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <Youtube url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-    </div>
-  );
+	return (
+		<div style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.4'}}>
+			<h1>Welcome to Remix</h1>
+			<ul>
+				<li>
+					<a
+						target="_blank"
+						href="https://remix.run/tutorials/blog"
+						rel="noreferrer"
+					>
+						15m Quickstart Blog Tutorial
+					</a>
+				</li>
+				<li>
+					<a
+						target="_blank"
+						href="https://remix.run/tutorials/jokes"
+						rel="noreferrer"
+					>
+						Deep Dive Jokes App Tutorial
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+						Remix Docs
+					</a>
+				</li>
+			</ul>
+			<Youtube muted playing url="https://www.youtube.com/watch?v=HHBbWFd02_A"/>
+		</div>
+	);
 }
