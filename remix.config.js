e59d22657a8cfb5ee1282e91203594144da818f6
Koki Takahashi <hakatasiloving@gmail.com>
2022-05-23 22:04:15 +0000

    Setup configuration for Cloudflare workers

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-	ignoredRouteFiles: ['**/.*'],
-	// appDirectory: "app",
-	// assetsBuildDirectory: "public/build",
-	// serverBuildPath: "build/index.js",
-	// publicPath: "/build/",
+  serverBuildTarget: "cloudflare-workers",
+  server: "./server.js",
+  devServerBroadcastDelay: 1000,
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
 };
