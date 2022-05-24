7b47682c146121a803f8a6435de9ccda2833698e
Koki Takahashi <hakatasiloving@gmail.com>
2022-05-24 19:46:37 +0000

    Implement server-side ping endpoint

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import {createEventHandler} from '@remix-run/cloudflare-workers';
 import * as build from '@remix-run/dev/server-build';
 
+const remixEventHandler = createEventHandler({build, mode: process.env.NODE_ENV});
+
 addEventListener(
 	'fetch',
-	createEventHandler({build, mode: process.env.NODE_ENV}),
+	(event) => {
+		const url = new URL(event.request.url);
+		const path = url.pathname;
+
+		if (path === '/ping') {
+			return event.respondWith(
+				new Response(Date.now().toString(), {
+					headers: {'Content-Type': 'text/plain;charset=UTF-8'},
+				}),
+			);
+		}
+
+		return remixEventHandler(event);
+	},
 );
