67326457997f9dc41d2e27e003ca0cedb033a022
Koki Takahashi <hakatasiloving@gmail.com>
2022-05-26 17:55:23 +0000

    Move time correction logics to clockSync

--- app/lib/clockSync.ts
+++ app/lib/clockSync.ts
@@ -7,9 +7,48 @@ export const clockOffsetState = atom<number>({
 });
 
 export class ClockSync {
+	private nextTimeoutId: NodeJS.Timer;
+
+	private measuredClockOffsets: number[];
+
 	constructor() {
-		setTimeout(() => {
-			setRecoil(clockOffsetState, 100);
+		this.measuredClockOffsets = [];
+		this.nextTimeoutId = setTimeout(() => {
+			this.probe();
 		}, 1000);
+	}
+
+	async probe() {
+		const {fetchStart, fetchEnd, fetchServerStart} = await this.ping();
+		const clockOffset = ((fetchServerStart - fetchStart) + (fetchServerStart - fetchEnd)) / 2;
+		this.measuredClockOffsets.push(clockOffset);
+
+		if (this.measuredClockOffsets.length >= 5) {
+			this.measuredClockOffsets = this.measuredClockOffsets.slice(-5);
+			// eslint-disable-next-line prefer-destructuring
+			const finalClockOffset = this.measuredClockOffsets.sort()[2];
+			setRecoil(clockOffsetState, finalClockOffset);
+		}
+
+		this.nextTimeoutId = setTimeout(() => {
+			this.probe();
+		}, 1000);
+	}
+
+	async ping() {
+		const fetchStart = Date.now();
+		const res = await fetch('/ping');
+		const fetchEnd = Date.now();
+
+		const text = await res.text();
+		const fetchServerStart = parseInt(text);
+
+		return {fetchStart, fetchEnd, fetchServerStart};
+	}
+
+	teardown() {
+		if (this.nextTimeoutId) {
+			clearTimeout(this.nextTimeoutId);
+		}
 	}
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,57 +1,18 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import {json} from '@remix-run/cloudflare';
-import {Link, useLoaderData} from '@remix-run/react';
+import {Link} from '@remix-run/react';
 import {useEffect, useState} from 'react';
 import {useRecoilValue} from 'recoil';
 import {clockOffsetState, ClockSync} from '~/lib/clockSync';
-
 // import Youtube from 'react-player/youtube';
 
-export const loader = async () => {
-	console.log('loading server-side time');
-
-	return json({
-		serverSideClock: Date.now(),
-	});
-};
-
-// eslint-disable-next-line react/function-component-definition, require-jsdoc
+// eslint-disable-next-line react/function-component-definition
 export default function Index() {
-	const {serverSideClock} = useLoaderData();
-
-	const [clientSideClock, setClientSideClock] = useState(0);
-	const [fetchStart, setFetchStart] = useState(0);
-	const [fetchEnd, setFetchEnd] = useState(0);
-	const [fetchServerStart, setFetchServerStart] = useState(0);
-	const [fetchServerEnd, setFetchServerEnd] = useState(0);
 	const [isBigCircle, setIsBigCircle] = useState(false);
-	const clockOffsetTarget = useRecoilValue(clockOffsetState);
-
-	useEffect(() => {
-		console.log('loading client-side time');
-		setClientSideClock(Date.now());
-	}, [setClientSideClock]);
-
-	useEffect(() => {
-		(async () => {
-			setFetchStart(Date.now());
-			const res = await fetch('/ping');
-			setFetchEnd(Date.now());
-
-			const dateHeader = res.headers.get('Date');
-			if (dateHeader) {
-				setFetchServerEnd(new Date(dateHeader).getTime());
-			}
-
-			const text = await res.text();
-			setFetchServerStart(parseInt(text));
-		})();
-	}, []);
+	const clockOffset = useRecoilValue(clockOffsetState);
 
 	useEffect(() => {
 		const intervalId = setInterval(() => {
-			const clockOffset = ((fetchServerStart - fetchStart) + (fetchServerStart - fetchEnd)) / 2;
 			const syncedTime = Date.now() + clockOffset;
 			const newIsBigCircle = Math.floor(syncedTime / 3000) % 2 === 0;
 			if (isBigCircle !== newIsBigCircle) {
@@ -61,19 +22,14 @@ export default function Index() {
 		return () => {
 			clearInterval(intervalId);
 		};
-	}, [isBigCircle, setIsBigCircle, fetchServerStart, fetchStart, fetchEnd]);
+	}, [isBigCircle, setIsBigCircle, clockOffset]);
 
 	useEffect(() => {
-		new ClockSync();
+		const clockSync = new ClockSync();
+		return () => {
+			clockSync.teardown();
+		};
 	}, []);
-
-	const offset = Math.min(
-		...[serverSideClock, clientSideClock, fetchStart, fetchEnd, fetchServerStart, fetchServerEnd]
-			.filter((t) => t !== 0),
-	);
-
-	const rtt = fetchEnd - fetchStart;
-	const clockOffset = ((fetchServerStart - fetchStart) + (fetchServerStart - fetchEnd)) / 2;
 
 	return (
 		<div style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.4'}}>
@@ -101,15 +57,7 @@ export default function Index() {
 					<Link to="/other">Other page</Link>
 				</li>
 			</ul>
-			<p>Server-side clock: +{serverSideClock - offset}ms</p>
-			<p>Client-side clock: +{clientSideClock - offset}ms</p>
-			<p>Ping client start: +{fetchStart - offset}ms</p>
-			<p>Ping server start: +{fetchServerStart - offset}ms</p>
-			<p>Ping server end: +{fetchServerEnd - offset}ms</p>
-			<p>Ping client end: +{fetchEnd - offset}ms</p>
-			<p>RTT: +{rtt}ms</p>
 			<p>Clock offset: {clockOffset}ms</p>
-			<p>Clock offset test: {clockOffsetTarget}ms</p>
 			<div style={{
 				width: '10rem',
 				height: '10rem',
