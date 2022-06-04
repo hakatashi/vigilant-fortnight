120f47ee623062412e2dc2e9e38380dbc32115b2
Koki Takahashi <hakatasiloving@gmail.com>
2022-06-04 18:36:08 +0900

    Implement signaling over SkyWay server

--- app/lib/PeerJsConnections.tsx
+++ app/lib/PeerJsConnections.tsx
@@ -1,6 +1,7 @@
-import type {Peer, DataConnection} from 'peerjs';
 import {useEffect, useState} from 'react';
 import {useHydrated} from 'remix-utils';
+import type {DataConnection} from 'skyway-js';
+import Peer from 'skyway-js';
 
 type Props = {
 	id: string,
@@ -12,31 +13,93 @@ class ConnectionManager {
 
 	peer: Peer | null = null;
 
-	connections: DataConnection[] = [];
+	connections: Map<string, {connection: DataConnection, interval: NodeJS.Timer}> = new Map();
 
 	async setId(id: string) {
 		if (id && id !== '') {
 			if (this.peer) {
 				this.peer.disconnect();
 			}
-			// eslint-disable-next-line no-shadow
-			const {Peer} = await import('peerjs');
-			this.peer = new Peer(id);
-			this.connections = Array.from(this.peerIds).map((peerId) => (
-				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-				this.peer!.connect(peerId)
-			));
-			console.log(`Connecting Peers as ID: ${id}`);
+
+			console.log(`PeerJS: Connecting Peers as ID: ${id}`);
+			this.peer = new Peer(id, {key: '59d6b4ef-a362-419d-a399-ab2c843cc583'});
+
+			this.peer.on('connection', (connection) => {
+				this.onIncomingConnection(connection);
+			});
+
+			const newId: string = await new Promise((resolve) => {
+				this.peer?.on('open', resolve);
+			});
+			console.log('PeerJS: New ID', newId);
+
+			for (const [peerId, {connection, interval}] of this.connections) {
+				if (interval) {
+					clearInterval(interval);
+				}
+				connection.close();
+				this.connections.delete(peerId);
+			}
+
+			for (const peerId of this.peerIds) {
+				this.connect(peerId);
+			}
 		}
 	}
 
+	connect(peerId: string) {
+		if (this.connections.has(peerId)) {
+			return;
+		}
+
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const connection = this.peer!.connect(peerId, {serialization: 'json'});
+		console.log(`PeerJS: Connecting to ${peerId}`);
+		connection.on('open', () => {
+			console.log(`PeerJS: Connected to ${peerId}`);
+		});
+		connection.on('data', (data) => {
+			console.log(`PeerJS: Received data from ${peerId}: ${data}`);
+		});
+		const interval = setInterval(() => {
+			if (connection.open) {
+				console.log(`PeerJS: Sending ping to ${peerId}`);
+				connection.send('ping');
+			}
+		}, 1000);
+		this.connections.set(peerId, {connection, interval});
+	}
+
+	onIncomingConnection(connection: DataConnection) {
+		console.log(`PeerJS: Incoming connection from ${connection.remoteId}`);
+
+		if (this.connections.has(connection.remoteId)) {
+			console.log('PeerJS: Incoming connection overwriting existing connection');
+			const existingConnection = this.connections.get(connection.remoteId);
+			existingConnection?.connection.close();
+			clearInterval(existingConnection?.interval);
+		}
+
+		connection.on('data', (data) => {
+			console.log(`PeerJS: Received data from ${connection.remoteId}: ${data}`);
+		});
+
+		const interval = setInterval(() => {
+			if (connection.open) {
+				console.log(`PeerJS: Sending ping to ${connection.remoteId}`);
+				connection.send('ping');
+			}
+		}, 1000);
+
+		this.connections.set(connection.id, {connection, interval});
+	}
+
 	setPeerIds(ids: string[]) {
-		for (const id of ids) {
-			if (!this.peerIds.has(id)) {
-				this.peerIds.add(id);
-				if (this.peer) {
-					this.connections.push(this.peer.connect(id));
-					console.log(`Connected to peer: ${id}`);
+		for (const peerId of ids) {
+			if (!this.peerIds.has(peerId)) {
+				this.peerIds.add(peerId);
+				if (this.peer && this.peer.open) {
+					this.connect(peerId);
 				}
 			}
 		}
